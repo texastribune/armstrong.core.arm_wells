0cff79746966efd7f822dfa1e5e60bd641926d11
matthewdylan <88053677+matthewdylan@users.noreply.github.com>
2024-03-11 14:57:39 -0500

    pushing new process with timing prints
    I'll remove the timing prints before pr'ing obv.

--- armstrong/core/arm_wells/managers.py
+++ armstrong/core/arm_wells/managers.py
@@ -1,11 +1,26 @@
 import datetime
+import time
 from django.db import models
 
 
 class WellManager(models.Manager):
-    def get_current(self, title=None, slug=None):
+    def get_current(self, title=None, titles=[], slug=None):
+        start_time = time.time()
         now = datetime.datetime.now()
         filter_params = dict(pub_date__lte=now, active=True)
+
+        if titles:
+            titles_dict = {}
+            filter_params['type__title__in'] = titles
+            results = (self.filter(**filter_params)
+                           .exclude(expire__lte=now, expires__isnull=False))
+            for title in titles:
+                titles_dict.update({'title': title, 'well': results.filter(type__title=title).latest('pub_date')})
+
+            end_time = time.time()
+            how_much_time = end_time - start_time
+            print(f'Took {how_much_time} to finish')
+            return titles_dict
 
         if (title):
             filter_params['type__title'] = title
@@ -13,7 +28,14 @@ class WellManager(models.Manager):
             filter_params['type__slug'] = slug
         else:
             raise self.model.DoesNotExist()
-
-        return (self.filter(**filter_params)
+        
+        result = (self.filter(**filter_params)
+                    .select_related('type__title', 'type__slug')
                     .exclude(expires__lte=now, expires__isnull=False)
                     .latest('pub_date'))
+
+        end_time = time.time()
+        how_much_time = end_time - start_time
+        print(f'Took {how_much_time} to finish')
+
+        return result
