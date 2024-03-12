750ead5f3a4e246759a1d2137b87d618828cb878
matthewdylan <88053677+matthewdylan@users.noreply.github.com>
2024-03-12 09:36:54 -0500

    moving back to dict after all
    should be easier to ref since we'll already have the keys... also removing all the time checking business

--- armstrong/core/arm_wells/managers.py
+++ armstrong/core/arm_wells/managers.py
@@ -1,27 +1,21 @@
 import datetime
-import time
 from django.db import models
 
 
 class WellManager(models.Manager):
     def get_current(self, title=None, titles=[], slug=None):
-        start_time = time.time()
         now = datetime.datetime.now()
         filter_params = dict(pub_date__lte=now, active=True)
 
         if titles:
-            print(titles)
-            titles_dict = []
+            titles_dict = {}
             filter_params['type__title__in'] = titles
             results = (self.filter(**filter_params)
                            .select_related('type')
                            .exclude(expires__lte=now, expires__isnull=False))
             for title in titles:
-                titles_dict.append({'title': title, 'well': results.filter(type__title=title).latest('pub_date')})
+                titles_dict.update({title: results.filter(type__title=title).latest('pub_date')})
 
-            end_time = time.time()
-            how_much_time = end_time - start_time
-            print(f'Took {how_much_time} to finish')
             return titles_dict
 
         if (title):
@@ -34,9 +28,5 @@ class WellManager(models.Manager):
         result = (self.filter(**filter_params)
                     .exclude(expires__lte=now, expires__isnull=False)
                     .latest('pub_date'))
-
-        end_time = time.time()
-        how_much_time = end_time - start_time
-        print(f'Took {how_much_time} to finish')
 
         return result
